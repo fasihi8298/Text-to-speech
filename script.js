fa7aac04252b04d6162de9d895e17d3806f016c4
Fasihi Ali <153727111+fasihi8298@users.noreply.github.com>
2024-06-12 16:39:31 +0500

    click function

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ window.speechSynthesis.onvoiceschanged = () => {
     speech.voice = voices[0];
 
     voicesSelect.innerHTML = ""; // clear the select options
-}
+
+    voices.forEach((voice, i) => {
+        let option = document.createElement("option");
+        option.text = voice.name + " (" + voice.lang + ")";
+        option.value = i;
+        voicesSelect.add(option);
+    });
+};
+
+document.querySelector("button").addEventListener("click", () =>{
+    speech.text = document.querySelector("textarea").value;
+    speech.voice = voices[voicesSelect.value]; // set the voice based on the select value
+    window.speechSynthesis.speak(speech);
+});
